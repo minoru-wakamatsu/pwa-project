344d89466dcaa7b7578e0dc316aa4130d70387c8
minoru.wakamatsu <wakamatsu.minoru@isopra.co.jp>
2021-09-02 11:34:02 +0900

    test7

---
+++ docs/precache-manifest.39ea34574e2402f1e00c3bd607d3ef67.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a168f0b5f5965d7b78e3",
+    "url": "/pwa-project/css/app.7935d263.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/pwa-project/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "88a0de61837996e3ceaeb34ec8672319",
+    "url": "/pwa-project/index.html"
+  },
+  {
+    "revision": "a3a6e412255691529c2f",
+    "url": "/pwa-project/js/about.70c8d2f9.js"
+  },
+  {
+    "revision": "a168f0b5f5965d7b78e3",
+    "url": "/pwa-project/js/app.6eb042c5.js"
+  },
+  {
+    "revision": "03b59126e041659a9e8c",
+    "url": "/pwa-project/js/chunk-vendors.2f2298b9.js"
+  },
+  {
+    "revision": "b0242c7d86e721f2ec8b72ffd225dac0",
+    "url": "/pwa-project/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pwa-project/robots.txt"
+  }
+]);
