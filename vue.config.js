fe92e470cd59d7e8e1e28ea8e0769af03f0c4198
minoru.wakamatsu <wakamatsu.minoru@isopra.co.jp>
2021-09-02 18:55:19 +0900

    test

--- vue.config.js
+++ vue.config.js
@@ -4,10 +4,10 @@ module.exports = {
   //     ? "./" //任意
   //     : "./",
   //   },
-  devServer: {
-    proxy: "http://localhost:3000",
-    //proxy: "https://aac4-150-249-204-198.ngrok.io",
-  },
+  //   devServer: {
+  //     proxy: "http://localhost:3000",
+  //     //proxy: "https://aac4-150-249-204-198.ngrok.io",
+  //   },
   publicPath: "/pwa-project",
   outputDir: "docs",
   pwa: {
