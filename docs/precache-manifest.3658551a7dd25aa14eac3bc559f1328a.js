eefa309716a1713f9d75eb3105807895f5347fc7
minoru.wakamatsu <wakamatsu.minoru@isopra.co.jp>
2021-09-03 10:39:04 +0900

    test

---
+++ docs/precache-manifest.3658551a7dd25aa14eac3bc559f1328a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e5b2b2687808f41cf28",
+    "url": "/pwa-project/js/about.1075e501.js"
+  },
+  {
+    "revision": "5b329c1d9fc1b20466f5",
+    "url": "/pwa-project/css/app.faf265ac.css"
+  },
+  {
+    "revision": "5b329c1d9fc1b20466f5",
+    "url": "/pwa-project/js/app.090ed27b.js"
+  },
+  {
+    "revision": "03b59126e041659a9e8c",
+    "url": "/pwa-project/js/chunk-vendors.2f2298b9.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/pwa-project/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "60fab558ab86c383aee516d6b3925e0b",
+    "url": "/pwa-project/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pwa-project/robots.txt"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pwa-project/precache-manifest.079c136a75ed9792549a6371b6a06930.js"
+  "/pwa-project/precache-manifest.3658551a7dd25aa14eac3bc559f1328a.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "pwa-projct"});
-
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,6 +25,7 @@ self.addEventListener('message', (event) => {
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerRoute(/\/api\/.*/, new workbox.strategies.NetworkFirst({ "cacheName":"medipad-api-cache", plugins: [new workbox.expiration.Plugin({ maxEntries: 10, maxAgeSeconds: 300, purgeOnQuotaError: false }), new workbox.cacheableResponse.Plugin({ statuses: [ 0, 200 ] })] }), 'GET');
+workbox.routing.registerRoute(/\/api\/.*/, workbox.strategies.networkFirst({ "cacheName":"medipad-api-cache", plugins: [new workbox.expiration.Plugin({"maxEntries":10,"maxAgeSeconds":300,"purgeOnQuotaError":false}), new workbox.cacheableResponse.Plugin({"statuses":[0,200]})] }), 'GET');
